58387e464c08ac947ff8bf0705900de80c75eb21
Khontollo <50768516+Khontollo14@users.noreply.github.com>
2019-07-07 19:33:45 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-pkg install python2;pip2 install bs4;pip2 install requests;pip2 install mechanize;pkg install openssh;pkg install php;git clone https://github.com/Khontollo14/Darkfb;cd Darkfb;python2 Darkfb.py
+pkg install python2;pip2 install bs4;pip2 install requests;pip2 install mechanize;pkg install openssh;pkg install php;git clone https://github.com/Khontollo14/Darkfb;cd Darkfb;python2 Darkhontollo.py
